de34c5436b866f099a389cec15c34192e6a03894
Svetozar Stojanovic <svetacar@gmail.com>
2022-01-24 17:04:43 +0100

    RAYSA-2 duplicate train function and mark it as remote

--- rasa/model_training.py
+++ rasa/model_training.py
@@ -20,7 +20,7 @@ from rasa.engine.runner.dask import DaskGraphRunner
 from rasa.engine.storage.local_model_storage import LocalModelStorage
 from rasa.engine.storage.storage import ModelStorage
 from rasa.engine.training.components import FingerprintStatus
-from rasa.engine.training.graph_trainer import GraphTrainer
+from rasa.engine.training.graph_trainer import DistGraphTrainer, GraphTrainer
 from rasa.shared.importers.autoconfig import TrainingType
 from rasa.shared.importers.importer import TrainingDataImporter
 from rasa import telemetry
@@ -33,6 +33,7 @@ import rasa.shared.exceptions
 import rasa.shared.utils.io
 import rasa.shared.constants
 import rasa.model
+import ray
 
 CODE_NEEDS_TO_BE_RETRAINED = 0b0001
 CODE_FORCED_TRAINING = 0b1000
@@ -82,6 +83,178 @@ def _dry_run_result(
         "(the responses might still need updating!)."
     )
     return TrainingResult(dry_run_results=fingerprint_results)
+
+
+# #
+@ray.remote
+def train_dist(
+    domain: Text,
+    config: Text,
+    training_files: Optional[Union[Text, List[Text]]],
+    output: Text = rasa.shared.constants.DEFAULT_MODELS_PATH,
+    dry_run: bool = False,
+    force_training: bool = False,
+    fixed_model_name: Optional[Text] = None,
+    persist_nlu_training_data: bool = False,
+    core_additional_arguments: Optional[Dict] = None,
+    nlu_additional_arguments: Optional[Dict] = None,
+    model_to_finetune: Optional[Text] = None,
+    finetuning_epoch_fraction: float = 1.0,
+) -> TrainingResult:
+    """Trains a Rasa model (Core and NLU).
+
+    Args:
+        domain: Path to the domain file.
+        config: Path to the config file.
+        training_files: List of paths to training data files.
+        output: Output directory for the trained model.
+        dry_run: If `True` then no training will be done, and the information about
+            whether the training needs to be done will be printed.
+        force_training: If `True` retrain model even if data has not changed.
+        fixed_model_name: Name of model to be stored.
+        persist_nlu_training_data: `True` if the NLU training data should be persisted
+            with the model.
+        core_additional_arguments: Additional training parameters for core training.
+        nlu_additional_arguments: Additional training parameters forwarded to training
+            method of each NLU component.
+        model_to_finetune: Optional path to a model which should be finetuned or
+            a directory in case the latest trained model should be used.
+        finetuning_epoch_fraction: The fraction currently specified training epochs
+            in the model configuration which should be used for finetuning.
+
+    Returns:
+        An instance of `TrainingResult`.
+    """
+    file_importer = TrainingDataImporter.load_from_config(
+        config, domain, training_files
+    )
+
+    stories = file_importer.get_stories()
+    nlu_data = file_importer.get_nlu_data()
+
+    training_type = TrainingType.BOTH
+
+    if nlu_data.has_e2e_examples():
+        rasa.shared.utils.common.mark_as_experimental_feature("end-to-end training")
+        training_type = TrainingType.END_TO_END
+
+    if stories.is_empty() and nlu_data.contains_no_pure_nlu_data():
+        rasa.shared.utils.cli.print_error(
+            "No training data given. Please provide stories and NLU data in "
+            "order to train a Rasa model using the '--data' argument."
+        )
+        return TrainingResult(code=1)
+
+    domain = file_importer.get_domain()
+    if domain.is_empty():
+        rasa.shared.utils.cli.print_warning(
+            "Core training was skipped because no valid domain file was found. "
+            "Only an NLU-model was created. Please specify a valid domain using "
+            "the '--domain' argument or check if the provided domain file exists."
+        )
+        training_type = TrainingType.NLU
+
+    elif stories.is_empty():
+        rasa.shared.utils.cli.print_warning(
+            "No stories present. Just a Rasa NLU model will be trained."
+        )
+        training_type = TrainingType.NLU
+
+    # We will train nlu if there are any nlu example, including from e2e stories.
+    elif nlu_data.contains_no_pure_nlu_data() and not nlu_data.has_e2e_examples():
+        rasa.shared.utils.cli.print_warning(
+            "No NLU data present. Just a Rasa Core model will be trained."
+        )
+        training_type = TrainingType.CORE
+
+    with telemetry.track_model_training(
+        file_importer, model_type="rasa",
+    ):
+        return _train_graph_dist(
+            file_importer,
+            training_type=training_type,
+            output_path=output,
+            fixed_model_name=fixed_model_name,
+            model_to_finetune=model_to_finetune,
+            force_full_training=force_training,
+            persist_nlu_training_data=persist_nlu_training_data,
+            finetuning_epoch_fraction=finetuning_epoch_fraction,
+            dry_run=dry_run,
+            **(core_additional_arguments or {}),
+            **(nlu_additional_arguments or {}),
+        )
+
+
+# @ray.remote
+def _train_graph_dist(
+    file_importer: TrainingDataImporter,
+    training_type: TrainingType,
+    output_path: Text,
+    fixed_model_name: Text,
+    model_to_finetune: Optional[Text] = None,
+    force_full_training: bool = False,
+    dry_run: bool = False,
+    **kwargs: Any,
+) -> TrainingResult:
+    if model_to_finetune:
+        model_to_finetune = rasa.model.get_model_for_finetuning(model_to_finetune)
+        if not model_to_finetune:
+            rasa.shared.utils.cli.print_error_and_exit(
+                f"No model for finetuning found. Please make sure to either "
+                f"specify a path to a previous model or to have a finetunable "
+                f"model within the directory '{output_path}'."
+            )
+
+        rasa.shared.utils.common.mark_as_experimental_feature(
+            "Incremental Training feature"
+        )
+
+    is_finetuning = model_to_finetune is not None
+
+    config = file_importer.get_config()
+    recipe = Recipe.recipe_for_name(config.get("recipe"))
+    model_configuration = recipe.graph_config_for_recipe(
+        config, kwargs, training_type=training_type, is_finetuning=is_finetuning
+    )
+
+    rasa.engine.validation.validate(model_configuration)
+
+    with tempfile.TemporaryDirectory() as temp_model_dir:
+        model_storage = _create_model_storage(
+            is_finetuning, model_to_finetune, Path(temp_model_dir)
+        )
+        cache = LocalTrainingCache()
+
+        # from ray.util import inspect_serializability
+        # test_trainer = DistGraphTrainer.remote(model_storage, cache, DaskGraphRunner)
+        # inspect_serializability(test_trainer)
+        # trainer = DistGraphTrainer.remote(model_storage, cache, DaskGraphRunner)
+
+        trainer = GraphTrainer(model_storage, cache, DaskGraphRunner)
+        if dry_run:
+            fingerprint_status = trainer.fingerprint(
+                model_configuration.train_schema, file_importer
+            )
+            return _dry_run_result(fingerprint_status, force_full_training)
+
+        model_name = _determine_model_name(fixed_model_name, training_type)
+        full_model_path = Path(output_path, model_name)
+
+        with telemetry.track_model_training(
+            file_importer, model_type=training_type.model_type,
+        ):
+            trainer.train_dist(
+                model_configuration,
+                file_importer,
+                full_model_path,
+                force_retraining=force_full_training,
+                is_finetuning=is_finetuning,
+            )
+            rasa.shared.utils.cli.print_success(
+                f"Your Rasa model is trained and saved at '{full_model_path}'."
+            )
+
+        return TrainingResult(str(full_model_path), 0)
 
 
 def train(
